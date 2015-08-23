c01435aee88a9fd4a1ace246445e6dc22116c81f
Max <webengineer.max@gmail.com>
2015-08-23 08:05:11 +0300

    Add integration tests for repo

---
+++ test/ecto_test.exs
@@ -0,0 +1,1 @@
+Code.require_file "../deps/ecto/integration_test/cases/repo.exs", __DIR__
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,33 @@
-ExUnit.start()
+Logger.configure(level: :info)
+
+ExUnit.start
+
+Application.put_env(:ecto, :primary_key_type, :binary_id)
+
+Code.require_file "../deps/ecto/integration_test/support/repo.exs", __DIR__
+
+defmodule Ecto.Integration.Case do
+  use ExUnit.CaseTemplate
+
+  setup_all do
+    # Run with schema in RAM to avoid clashing with server
+    :mnesia.start
+  end
+end
+
+alias Ecto.Integration.TestRepo
+
+Application.put_env(:ecto, TestRepo,
+                    adapter: Mnesia.Ecto)
+
+defmodule Ecto.Integration.TestRepo do
+  use Ecto.Integration.Repo, otp_app: :ecto
+end
+
+# Load up the repository, start it, and run migrations
+_   = Ecto.Storage.down(TestRepo)
+:ok = Ecto.Storage.up(TestRepo)
+
+{:ok, _pid} = TestRepo.start_link
+
+:ok = Ecto.Migrator.up(TestRepo, 0, Ecto.Integration.Migration, log: false)
