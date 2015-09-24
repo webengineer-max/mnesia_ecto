97cacb4a03e7a027d0924ce09c23f80888db60d2
Max <webengineer.max@gmail.com>
2015-09-24 10:50:51 +0300

    Freeze ecto

--- mix.exs
+++ mix.exs
@@ -27,6 +27,6 @@ defmodule Mnesia.Ecto.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    [{:ecto, git: "git@github.com:elixir-lang/ecto.git"}]
+    [{:ecto, "~> 1.0"}]
   end
 end
